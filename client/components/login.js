b48e0632826d5bb2709162aa5e85816e81378f72
Megan W <contactmeganwalsh@gmail.com>
2019-11-21 16:25:07 -0500

    feat: login component ...in progress

---
+++ client/components/login.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { Text, TextInput, View, Button, StyleSheet } from "react-native"
+import {connect} from 'react-redux'
+
+export default class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      username: "",
+      password: "",
+      error: false
+    }
+    this.submitLogin = this.submitLogin.bind(this)
+  }
+
+  submitLogin(e) {
+    this.setState({
+      error: true
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 27 }}>LOGIN</Text>
+        <TextInput
+          placeholder="Username"
+          onChangeText={text => this.setState({ username: text })}
+          value={this.state.username}
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={text => this.setState({ password: text })}
+          secureTextEntry={true} 
+          value={this.state.password}
+        />
+        <View style={{ margin: 7 }} />
+        <Text>{this.state.error? "Incorrent username or password": "" }</Text>
+        <Button title="Sumbit" onPress={this.submitLogin}/>
+      </View> 
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  errorMessageText: {
+    textDecorationColor: 'red'
+  }
+})
+
+
+const mapDispatch = dispatch => {
+  // Login Data
+}
