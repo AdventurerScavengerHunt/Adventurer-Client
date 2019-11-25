22bc448d35e25ceb1f54b3c4ba8795ca3f23355d
JWaltuch <jenna.waltuch@gmail.com>
2019-11-25 15:20:44 -0500

    playing with navigation

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import React from "react"
-import { StyleSheet, Text, View } from "react-native"
-import Login from "./client/components/login"
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './client/components/login';
+import MapScreen from './client/components/map-screen';
 
-import { Provider } from "react-redux"
-import store from "./client/store"
+import { Provider } from 'react-redux';
+import store from './client/store';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: Login,
+  },
+  MapScreen: {
+    screen: MapScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
   return (
-    <Provider store={store}><Login></Login></Provider>
-  )
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-})
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
