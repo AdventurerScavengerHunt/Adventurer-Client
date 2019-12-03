0880c1dc0b79fbec99c1f8f95a447e661f6ada33
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2019-12-03 17:34:48 -0500

    style: migrated styling from react-native to expo

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './client/components/login';
-import MapScreen from './client/components/map-screen';
-import StartScreen from './client/components/start-screen';
-import SignUp from './client/components/signup';
-import HuntScreen from './client/components/hunts';
+import React, {Component} from 'react'
+import Login from './client/components/login'
+import MapScreen from './client/components/map-screen'
+import StartScreen from './client/components/start-screen'
+import SignUp from './client/components/signup'
+import HuntScreen from './client/components/hunts'
+import InstructionScreen from './client/components/instructions'
 
-import { Provider } from 'react-redux';
-import store from './client/store';
+import {Provider} from 'react-redux'
+import store from './client/store'
 
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
 
 const AppNavigator = createStackNavigator(
   {
@@ -19,27 +19,19 @@ const AppNavigator = createStackNavigator(
     StartScreen: StartScreen,
     HuntScreen: HuntScreen,
     MapScreen: MapScreen,
+    InstructionScreen: InstructionScreen
   },
   {
-    initialRouteName: 'Login',
+    initialRouteName: 'Login'
   }
-);
+)
 
-const AppContainer = createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator)
 
 export default function App() {
   return (
     <Provider store={store}>
       <AppContainer />
     </Provider>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
